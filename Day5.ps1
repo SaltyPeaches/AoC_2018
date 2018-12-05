705722961ce398c01a19c306a0dd10b7ced8ed08
Brett Anderson <andersonbrett@outlook.com>
2018-12-05 11:28:12 -0500

    Added Day 5

---
+++ Day5.ps1
@@ -0,0 +1,40 @@
+$in = Get-Content $PSScriptRoot\Inputs\Day5.txt
+$alphabetsoup = [string[]][char[]](97..122)
+
+function ReactPolymer{
+    param(
+        $polymer
+    )
+
+    $length = ''
+
+    while($polymer.length -ne $length){
+        $length = $polymer.length
+        foreach($n in $alphabetsoup){
+            $polymer = $polymer -creplace ($n.toLower()+$n.toUpper()),''
+            $polymer = $polymer -creplace ($n.toUpper()+$n.toLower()),''
+        }
+    }
+
+    return $polymer.Length
+}
+
+# Part 1
+$part1 = ReactPolymer $in
+
+# Part 2
+$total = $alphabetsoup.Length
+$loop = 0
+$lengths = foreach($noodle in $alphabetsoup){
+    $loop++
+    write-host "Loop $loop of $total - Letter $noodle"
+    [PSCustomObject]@{
+        Letter = $noodle
+        Length = (ReactPolymer ($in -replace $noodle))
+    } 
+}
+
+$part2 = $lengths | sort Length -desc | select -expandproperty Length -last 1
+
+write-host "Part 1: $part1"
+write-host "Part 2: $part2"
